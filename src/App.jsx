5a3641f63b009d0385b890329ae189e7eee9a7fb
TheCats100 <m-g-152@outlook.fr>
2020-06-03 18:16:52 +0200

    fix(all): git bug

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './components/navbar/NavBar'
+
+function App() {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/Test.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+function test() {
+    return(
+        <div>
+            <p>coucou</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt ab culpa odit iste iure inventore ut soluta libero, sunt quos nesciunt! Quidem dolorum quaerat sint, odit quo debitis nemo quasi.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt ab culpa odit iste iure inventore ut soluta libero, sunt quos nesciunt! Quidem dolorum quaerat sint, odit quo debitis nemo quasi.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt ab culpa odit iste iure inventore ut soluta libero, sunt quos nesciunt! Quidem dolorum quaerat sint, odit quo debitis nemo quasi.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt ab culpa odit iste iure inventore ut soluta libero, sunt quos nesciunt! Quidem dolorum quaerat sint, odit quo debitis nemo quasi.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt ab culpa odit iste iure inventore ut soluta libero, sunt quos nesciunt! Quidem dolorum quaerat sint, odit quo debitis nemo quasi.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt ab culpa odit iste iure inventore ut soluta libero, sunt quos nesciunt! Quidem dolorum quaerat sint, odit quo debitis nemo quasi.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt ab culpa odit iste iure inventore ut soluta libero, sunt quos nesciunt! Quidem dolorum quaerat sint, odit quo debitis nemo quasi.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt ab culpa odit iste iure inventore ut soluta libero, sunt quos nesciunt! Quidem dolorum quaerat sint, odit quo debitis nemo quasi.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt ab culpa odit iste iure inventore ut soluta libero, sunt quos nesciunt! Quidem dolorum quaerat sint, odit quo debitis nemo quasi.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt ab culpa odit iste iure inventore ut soluta libero, sunt quos nesciunt! Quidem dolorum quaerat sint, odit quo debitis nemo quasi.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt ab culpa odit iste iure inventore ut soluta libero, sunt quos nesciunt! Quidem dolorum quaerat sint, odit quo debitis nemo quasi.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt ab culpa odit iste iure inventore ut soluta libero, sunt quos nesciunt! Quidem dolorum quaerat sint, odit quo debitis nemo quasi.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt ab culpa odit iste iure inventore ut soluta libero, sunt quos nesciunt! Quidem dolorum quaerat sint, odit quo debitis nemo quasi.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt ab culpa odit iste iure inventore ut soluta libero, sunt quos nesciunt! Quidem dolorum quaerat sint, odit quo debitis nemo quasi.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt ab culpa odit iste iure inventore ut soluta libero, sunt quos nesciunt! Quidem dolorum quaerat sint, odit quo debitis nemo quasi.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt ab culpa odit iste iure inventore ut soluta libero, sunt quos nesciunt! Quidem dolorum quaerat sint, odit quo debitis nemo quasi.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt ab culpa odit iste iure inventore ut soluta libero, sunt quos nesciunt! Quidem dolorum quaerat sint, odit quo debitis nemo quasi.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt ab culpa odit iste iure inventore ut soluta libero, sunt quos nesciunt! Quidem dolorum quaerat sint, odit quo debitis nemo quasi.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt ab culpa odit iste iure inventore ut soluta libero, sunt quos nesciunt! Quidem dolorum quaerat sint, odit quo debitis nemo quasi.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt ab culpa odit iste iure inventore ut soluta libero, sunt quos nesciunt! Quidem dolorum quaerat sint, odit quo debitis nemo quasi.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt ab culpa odit iste iure inventore ut soluta libero, sunt quos nesciunt! Quidem dolorum quaerat sint, odit quo debitis nemo quasi.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt ab culpa odit iste iure inventore ut soluta libero, sunt quos nesciunt! Quidem dolorum quaerat sint, odit quo debitis nemo quasi.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt ab culpa odit iste iure inventore ut soluta libero, sunt quos nesciunt! Quidem dolorum quaerat sint, odit quo debitis nemo quasi.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt ab culpa odit iste iure inventore ut soluta libero, sunt quos nesciunt! Quidem dolorum quaerat sint, odit quo debitis nemo quasi.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt ab culpa odit iste iure inventore ut soluta libero, sunt quos nesciunt! Quidem dolorum quaerat sint, odit quo debitis nemo quasi.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt ab culpa odit iste iure inventore ut soluta libero, sunt quos nesciunt! Quidem dolorum quaerat sint, odit quo debitis nemo quasi.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt ab culpa odit iste iure inventore ut soluta libero, sunt quos nesciunt! Quidem dolorum quaerat sint, odit quo debitis nemo quasi.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt ab culpa odit iste iure inventore ut soluta libero, sunt quos nesciunt! Quidem dolorum quaerat sint, odit quo debitis nemo quasi.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt ab culpa odit iste iure inventore ut soluta libero, sunt quos nesciunt! Quidem dolorum quaerat sint, odit quo debitis nemo quasi.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt ab culpa odit iste iure inventore ut soluta libero, sunt quos nesciunt! Quidem dolorum quaerat sint, odit quo debitis nemo quasi.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt ab culpa odit iste iure inventore ut soluta libero, sunt quos nesciunt! Quidem dolorum quaerat sint, odit quo debitis nemo quasi.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt ab culpa odit iste iure inventore ut soluta libero, sunt quos nesciunt! Quidem dolorum quaerat sint, odit quo debitis nemo quasi.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt ab culpa odit iste iure inventore ut soluta libero, sunt quos nesciunt! Quidem dolorum quaerat sint, odit quo debitis nemo quasi.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt ab culpa odit iste iure inventore ut soluta libero, sunt quos nesciunt! Quidem dolorum quaerat sint, odit quo debitis nemo quasi.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt ab culpa odit iste iure inventore ut soluta libero, sunt quos nesciunt! Quidem dolorum quaerat sint, odit quo debitis nemo quasi.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt ab culpa odit iste iure inventore ut soluta libero, sunt quos nesciunt! Quidem dolorum quaerat sint, odit quo debitis nemo quasi.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt ab culpa odit iste iure inventore ut soluta libero, sunt quos nesciunt! Quidem dolorum quaerat sint, odit quo debitis nemo quasi.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt ab culpa odit iste iure inventore ut soluta libero, sunt quos nesciunt! Quidem dolorum quaerat sint, odit quo debitis nemo quasi.</p>
+
+        </div>
+    )
+}
+
+export default test;
