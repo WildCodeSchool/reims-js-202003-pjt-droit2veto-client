95502ec93bca3adc557e929a667b5c8c43546df4
angelique-m08 <mora.angelique@gmail.com>
2020-06-12 13:05:12 +0200

    fix(app): resolved conflits

--- conf.js
+++
@@ -1,9 +0,0 @@
-const  mysql = require('mysql');
-const  connection = mysql.createConnection({
-  host :  'localhost',
-  user :  'angelique',
-  password :  'Bija2413!',
-  database :  'Droit2Veto',
-});
-
-module.exports = connection;
