ef7603019dcbaa37c27a5c2a2fc343a8fdb93b67
TheCats100 <m-g-152@outlook.fr>
2020-06-17 18:18:47 +0200

    feat(navbar): component widht and img cat/dog

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import NavBar from './components/navbar/NavBar';
 import {
   BrowserRouter as
-  Router,
+    Router,
   Switch,
   Route,
   Link,
 } from 'react-router-dom';
+import NavBar from './components/navbar/NavBar';
 
 
 import LoremIpsum from './components/test/LoremIpsum';
@@ -24,24 +24,28 @@ function App() {
           <p>Profil en cours</p>
         </div>
         <div className="navbarPage">
-          <NavBar />
-          <Switch>
-            <Route exact path="/">
-              <BlocTuto />
-            </Route>
-            <Route exact path="/informations">
-              <Information />
-            </Route>
-            <Route excat path="/anesthesia">
-              <LoremIpsum />
-            </Route>
-            <Route exact path="/activities">
-              <ActivityList />
-            </Route>
-            <Route exact path="/order">
-              <LoremIpsum />
-            </Route>
-          </Switch>
+          <div>
+            <NavBar />
+          </div>
+          <div className="appComponentsRouter">
+            <Switch>
+              <Route exact path="/">
+                <BlocTuto />
+              </Route>
+              <Route exact path="/informations">
+                <Information />
+              </Route>
+              <Route excat path="/anesthesia">
+                <LoremIpsum />
+              </Route>
+              <Route exact path="/activities">
+                <ActivityList />
+              </Route>
+              <Route exact path="/order">
+                <LoremIpsum />
+              </Route>
+            </Switch>
+          </div>
         </div>
       </Router>
     </>
