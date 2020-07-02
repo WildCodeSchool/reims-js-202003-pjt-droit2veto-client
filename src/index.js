547e55c140ca71364e708d80cca8afae57df78f0
WildSteph <Stephaine.Pantiez@gmail.com>
2020-07-02 11:37:05 +0200

    refactor(index): add redux tool

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import TokenReducer from './components/redux/TokenReducer';
 
 const store = createStore(
   TokenReducer,
-  window.REDUX_DEVTOOLS_EXTENSION && window.REDUX_DEVTOOLS_EXTENSION()
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 
