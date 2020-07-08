63f607826ecdf54a50fa3bb1a70cd4587d459940
WildSteph <Stephaine.Pantiez@gmail.com>
2020-07-08 16:19:05 +0200

    refactor(admin): add admin post

--- src/components/activity/Admin.jsx
+++ src/components/activity/Admin.jsx
@@ -1,8 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
+import { connect } from 'react-redux';
 
-const Admin = () => (
-  <p>hello i am eux admin</p>
-);
+const mapStateToProps = (state) => ({
+  admin: state.admin,
+});
 
-export default Admin;
+const Admin = () => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
+  function post() {
+    Axios.post('http://localhost:8000/activities', { title, description }).then(
+      (res) => alert('enregistrement reussi'),
+    );
+  }
+
+  return (
+    <form onSubmit={(event) => {
+      post();
+      event.preventDefault();
+    }}
+    >
+      <div>
+        <label htmlFor="Title" />
+        <p>Titre de activité :</p>
+        <input className="logPageFormInput" type="title" name="title" id="title" placeholder="" onChange={(event) => setTitle(event.target.value)} required />
+      </div>
+      <div>
+        <label htmlFor="description" />
+        <p>Description de activité :</p>
+        <input className="logPageFormInput" type="text" name="text" id="text" placeholder="" onChange={(event) => setDescription(event.target.value)} required />
+      </div>
+      <button type="submit">validé</button>
+    </form>
+  );
+};
+
+export default connect(mapStateToProps)(Admin);
